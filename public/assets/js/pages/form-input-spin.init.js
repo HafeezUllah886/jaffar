f009e29622c3132e00c3ed38ce2a3a34ba63aaf0
HafeezUllah886 <55585864+HafeezUllah886@users.noreply.github.com>
2024-10-13 21:23:29 +0500

    oder design completed

--- public/assets/js/pages/form-input-spin.init.js
+++ public/assets/js/pages/form-input-spin.init.js
@@ -1,27 +1,21 @@
 $(document).ready(function() {
-    // Handle click events for all plus and minus buttons
-    $('[class^="plus"], [class^="minus"]').on('click', function() {
-        console.log("working");
+    function plus(id)
+    {
+        var qtyInput = $("#qty_"+id);
+        var currentValue = parseInt(qtyInput.val());
+        currentValue += 1;
+        qtyInput.val(currentValue);
+    }
 
-        // Extract the number from the class name (e.g., "plus-1" becomes "1")
-        var classNumber = $(this).attr('class').split('-')[1];
-        var $quantityInput = $(`.quantity-${classNumber}`);
-
-        var currentValue = parseInt($quantityInput.val());
-        var maxValue = parseInt($quantityInput.attr('max')) || Infinity;
-        var minValue = parseInt($quantityInput.attr('min')) || 1;
-        console.log(currentValue);
-        // Check if it's a plus button or minus button
-        if ($(this).hasClass('plus')) {
-            console.log('Plus');
-            if (currentValue < maxValue) {
-                $quantityInput.val(currentValue + 1);
-            }
-        } else if ($(this).hasClass('minus')) {
-            console.log('minus');
-            if (currentValue > minValue) {
-                $quantityInput.val(currentValue - 1);
-            }
+    function minus(id)
+    {
+        var qtyInput = $("#qty_"+id);
+        var currentValue = parseInt(qtyInput.val());
+        if(currentValue > 1)
+        {
+            currentValue -= 1;
         }
-    });
+
+        qtyInput.val(currentValue);
+    }
 });
